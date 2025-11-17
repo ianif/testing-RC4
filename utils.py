94527aa8d7d575e386783b458310ffdc91e22710
Ioanna Anyfanti <ioanna@turintech.ai>
2025-11-17 10:37:12 +0000

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,10 @@
+def calculate_discount(price, percent):
+    # logic bug:
+    # should be price * (percent / 100)
+    return price - percent
+
+def send_email():  # exists but does nothing
+    pass
+
+def unused():
+    return "hello"
